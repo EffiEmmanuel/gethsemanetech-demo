4a29285472ab95aa92fc2bdc29a3aa674c0b45d3
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-05-24 02:57:06 +0100

    INIT: Initial commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoLogoWhatsapp, IoMenuOutline } from "react-icons/io5";
+import { VscClose } from "react-icons/vsc";
+import { useState } from "react";
+import NavLink from "./NavLink";
+
+// Logos
+// import ShuttlelaneLogoWhite from "../../../assets/logos/shuttlelane-white.png";
+// import ShuttlelaneLogoColored from "../../../assets/logos/logo.png";
+import { Fade } from "react-awesome-reveal";
+import { useWindowScroll } from "@uidotdev/usehooks";
+import { BiLogoGmail, BiSolidPhone } from "react-icons/bi";
+import DropdownItem from "../DropdownItem";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+// Images
+import gethsemaneTechLogo from "../../assets/logos/gethsemane-technologies-logo.png";
+
+function NavBar(props) {
+  return (
+    <div className="relative z-[30]">
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+
+export default function Homepage() {
+  return (
+    <div className="">
+      <NavBar />
+      <h1 className="text-sm">Gethsemane Technologies</h1>
+    </div>
+  );
+}
